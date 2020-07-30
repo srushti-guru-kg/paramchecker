ff65846e18c06223e78e7ee4e763cc3df3df043d
srushti-guru-kg <68851540+srushti-guru-kg@users.noreply.github.com>
2020-07-30 14:48:04 +0530

    Update the cparamchecker.cpp
    changes in .cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,46 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+bool BpmOK(float bpm)
+{
+  if(bpm < 70 || bpm >150)
+  {
     return false;
   }
-  return true;
+  else
+  {
+    return true;
+  }
+}
+bool Spo2OK(float spo2)
+{
+  if(sp02 < 80)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+bool ResprateOK(float respRate)
+{
+  if(respRate < 30 || respRate > 60)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+
+bool vitalsAreOk(float bpm, float spo2, float respRate)
+{ 
+  if(BpmOK(bpm) && Spo2OK(spo2) && RespRateOK(respRate))
+  {
+    return true;
+  }
+  else
+  {
+    retrun false;
+  }
 }
