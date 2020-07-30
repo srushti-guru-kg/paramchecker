f68521c2f7786ab7e0d1749950ed8d7b1d1e2cdc
srushti-guru-kg <68851540+srushti-guru-kg@users.noreply.github.com>
2020-07-30 16:50:01 +0530

    update test file

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,10 +1,34 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
  
-TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
+TEST(BpmOK, BPM) { 
+    ASSERT_EQ(true, BpmOK(80,100));
 }
- 
+
+TEST(BpmOK, BPM) { 
+    ASSERT_EQ(false, BpmOK(50,170));
+}
+
+TEST(Spo2OK, SPO2) { 
+    ASSERT_EQ(true, Spo2OK(100));
+}
+
+TEST(Spo2OK, SPO2) { 
+    ASSERT_EQ(false, Spo2OK(50));
+}
+
+TEST(ResprateOK, respRate) { 
+    ASSERT_EQ(true, ResprateOK(20,100));
+}
+
+TEST(ResprateOK, respRate) { 
+    ASSERT_EQ(false, ResprateOK(40,50));
+}
+
+TEST(VitalsTest, SPO2) {
+    ASSERT_EQ(true, vitalsAreOk(80, 100, 50));
+}
+
 TEST(VitalsTest, SPO2) {
     ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
 }
